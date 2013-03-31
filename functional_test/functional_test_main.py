dc0355fbb1f40d99968d38b934b2568a0329a663
dougfort <dougfort@spideroak.com>
2013-03-30 20:45:14 -0400

    started on functional_test_main

--- functional_test/functional_test_main.py
+++ functional_test/functional_test_main.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-stdout_spewer.py
+functional_test_main.py
 
-A program to write log lines to stdout at intervals
+main control program for functional test of the Old Log Inn
 """
 import argparse
+import json
 import logging
 import sys
 
@@ -13,32 +14,44 @@ from old_log_inn.signal_handler import set_signal_handler
 _log_format_template = '%(asctime)s %(levelname)-8s %(name)-20s: %(message)s'
 
 def _parse_commandline():
-    parser = \
-        argparse.ArgumentParser(description='write log entries at intervals.')
+    parser = argparse.ArgumentParser(description='functional test.')
     parser.add_argument("-l", "--log-path", dest="log_path")
+    parser.add_argument("-c", "--config-path", dest="config_path")
     return parser.parse_args()
 
 def _initialize_logging(log_path):
-    handler = logging.StreamHandler(stream=sys.stdout)
+    log_level = logging.DEBUG
+    handler = logging.FileHandler(log_path, mode="w", encoding="utf-8")
     formatter = logging.Formatter(_log_format_template)
     handler.setFormatter(formatter)
+
     logging.root.addHandler(handler)
-    logging.root.setLevel(logging.DEBUG)
+    logging.root.setLevel(log_level)
+
+def _load_config_file(config_path):
+    log = logging.getLogger("_load_config_file")
+    log.info("loading config file '{0}'".format(config_path))
+    with open(config_path, "r") as input_file:
+        return json.load(input_file)
 
 def main():
     """
     main entry point
     """
     args = _parse_commandline()
-    _initialize_logging(args.log_path)
 
+    _initialize_logging(args.log_path)
     log = logging.getLogger("main")
+    log.info("program starts")
+
+    config = _load_config_file(args.config_path)
 
     halt_event = set_signal_handler()
     while not halt_event.is_set():
         log.info("glort")
         halt_event.wait(1.0)
 
+    log.info("program terminates wiht return code 0")
     return 0
 
 if __name__ == "__main__":
--- functional_test/log_spewer.py
+++ functional_test/log_spewer.py
@@ -6,24 +6,12 @@ A program to write log lines at intervals
 """
 import argparse
 import logging
-import signal 
 import sys
-from threading import Event
 
 from old_log_inn.zmq_push_log_handler import ZMQPushLogHandler
+from old_log_inn.signal_handler import set_signal_handler
 
 _log_format_template = '%(asctime)s %(levelname)-8s %(name)-20s: %(message)s'
-
-def _create_signal_handler(halt_event):
-    def cb_handler(*_):
-        halt_event.set()
-    return cb_handler
-
-def _set_signal_handler(halt_event):
-    """
-    set a signal handler to set halt_event when SIGTERM is raised
-    """
-    signal.signal(signal.SIGTERM, _create_signal_handler(halt_event))
 
 def _parse_commandline():
     parser = \
@@ -45,11 +33,9 @@ def main():
     args = _parse_commandline()
     _initialize_logging(args.log_path)
 
-    halt_event = Event()
-    _set_signal_handler(halt_event)
-
     log = logging.getLogger("main")
 
+    halt_event = set_signal_handler()
     while not halt_event.is_set():
         log.info("glort")
         halt_event.wait(1.0)
