2d4455de2c16d77e8a76b6701901c299f0bc3017
dougfort <dougfort@spideroak.com>
2013-04-04 08:49:44 -0400

    move functional test script into git

---
+++ functional_test/functional_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e 
+set -x 
+
+export PYTHONPATH="${HOME}/old_log_inn"
+python3.2 "${HOME}/old_log_inn/functional_test/functional_test_main.py" \
+    --log-path="/tmp/functional_test.log" \
+    --config-path="${HOME}/old_log_inn/functional_test/functional_test.json" \
+    --old-log-inn-path="${HOME}/old_log_inn"
