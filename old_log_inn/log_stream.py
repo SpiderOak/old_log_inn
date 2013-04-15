c58d6c3a4ddf2918ec68a3b08eb2e9afb0a8d918
dougfort <dougfort@spideroak.com>
2013-04-15 09:30:45 -0400

    wip log streams

--- old_log_inn/log_stream.py
+++ old_log_inn/log_stream.py
@@ -5,11 +5,12 @@ log_stream.py
 objects for managing log streams: LogStreamWriter, LogStreamReader
 """
 from datetime import datetime, timedelta
+from gzip import GzipFile
 import struct
 
-_control_format = "!BII"
-_control_size = struct.calcsize(_control_format)
-_control_protocol_version = 1
+_frame_format = "!BII"
+_frame_size = struct.calcsize(_frame_format)
+_frame_protocol_version = 1
 
 def _compute_timestamp(granularity, time_value):
     """
@@ -42,15 +43,74 @@ def _compute_timestamp(granularity, time_value):
 
 class LogStreamWriter(object):
     def __init__(self, granularity, work_dir, complete_dir):
-        self._granularity = granularity
+        self._granularity = timdelta(seconds=granularity)
+
         self._work_dir = work_dir
         self._complete_dir = complete_dir
 
+        self._output_timestamp = None
+        self._output_file_name = None
+
         self._output_file = None
+        self._output_gzip_file = None
 
     def write(self, header, data):
-        pass
+        timestamp = self._compute_current_timestamp()
+        self.check_for_rollover(timestamp)
 
+        if self._output_timestamp is None:
+            self._open_output_file(timestamp)
+
+        frame = struct.pack(_frame_format, 
+                            _frame_protocol_version, 
+                            len(header),
+                            len(data))
+        self._output_gzip_file.write(frame)
+        self._output_gzip_file.write(header)
+        self._output_gzip_file.write(data)
+        self.output_file.flush()
+
+    def check_for_rollover(self, timestamp=None):
+        """
+        check the timestamp on the currently open file (if any)
+        and close it if it has aged out
+        """
+        if self._output_timestamp is None:
+            return
+
+        if timestamp is None:
+            timestamp = self._compute_current_timestamp()
+
+        if timestamp != self._output_file_timestamp:
+            self._close_current_file()
+
+    def _compute_current_timestamp(self):
+        """
+        return the timestamp for the current moment
+        """
+        return _compute_timestamp(self._granularity, datetime.utcnow())
+
+    def _close_current_file(self):
+        self._output_gzip_file.close()
+        self._output_gzip_file = None
+
+        self._output_file.close()
+        self._output_file = None
+
+        work_path = os.path.join(self._work_dir, self._output_file_name)
+        complete_path = os.path.join(self._complete_dir, 
+                                     self._output_file_name)
+        os.rename(work_path, complete_path)
+
+        self._output_timestamp = None
+        self._output_file_name = None
+
+    def _open_output_file(self, timestamp):
+        self._output_timestamp = timestamp
+        self._output_file_name = ".".join(timestamp, "gz")
+        work_path = os.path.join(self._work_dir, self._output_file_name)
+        self._output_file = open(work_path, "wb")
+        self._output_gzip_file = GzipFile(fileobj=self._output_file)
 
 class LogStreamReader(object):
     def __init__(self, stream_dir):
